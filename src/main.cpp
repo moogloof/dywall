8d59ad5c61919fe8586407a85d34a966333d95ff
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-10-08 17:36:57 -0700

    Speed over setup time

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 	// Open video to capture
 	cv::VideoCapture vidcap(argv[1]);
 	// The filename for updating
-	char temp_vid[10];
+	char temp_vid[20];
 	// The command string
 	char cmd[512];
 	// VideoCapture fps
@@ -23,29 +23,42 @@ int main(int argc, char* argv[]) {
 
 	// Switching index to update the background
 	int i = 0;
+	int framelen = 0;
 
 	// Get the current working directory
 	char cwd[256];
 	getcwd(cwd, sizeof(cwd));
 
+	// Create video frame folder
+	system("mkdir -p flips; cd flips");
+
 	// Loop through video
+	std::cout << "Flattening video...";
 	while (1) {
 		// Read the video
 		vidcap >> frame;
-		i = (++i) % 2;
+		i++;
+		framelen++;
 
 		if (frame.empty()) {
-			// Reset if video is done
-			vidcap.set(cv::CAP_PROP_POS_AVI_RATIO, 0);
+			break;
 		} else {
 			// Write the frame to video
-			std::sprintf(temp_vid, "flip%d.jpg", i);
+			std::sprintf(temp_vid, "flips/flip%d.jpg", i);
 			cv::imwrite(temp_vid, frame);
-
-			// Update the background
-			std::sprintf(cmd, "osascript -e 'tell app \"Finder\" to set desktop picture to POSIX file \"%s/%s\"'", cwd, temp_vid);
-			system(cmd);
 		}
+	}
+
+	std::cout << "\rFinished flattening video." << std::endl;
+	std::cout << "Starting animation." << std::endl;
+
+	i = 0;
+	while (1) {
+		i = (++i) % framelen;
+		// Update the background
+		std::sprintf(temp_vid, "flip%d.jpg", i);
+		std::sprintf(cmd, "osascript -e 'tell app \"Finder\" to set desktop picture to POSIX file \"%s/flips/%s\"'", cwd, temp_vid);
+		system(cmd);
 	}
 
 	return 0;
